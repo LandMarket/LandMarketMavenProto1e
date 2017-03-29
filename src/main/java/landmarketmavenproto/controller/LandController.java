21fa5b90263970957b8c91696db4b1ecf4400cfd
Vitya Ezersky <vitya.ezersky95@mail.ru>
2017-03-29 10:48:42 +0300

    new

--- src/main/java/landmarketmavenproto/controller/LandController.java
+++ src/main/java/landmarketmavenproto/controller/LandController.java
@@ -2,11 +2,12 @@ package landmarketmavenproto.controller;
 
 import landmarketmavenproto.model.Land;
 import landmarketmavenproto.repository.LandRepository;
+import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -20,49 +21,66 @@ public class LandController {
     LandRepository lrepository;
 
     @RequestMapping(method = RequestMethod.POST)
-    public Map<String, Object> createLand(@RequestBody Map<String, Object> landMap){
-        Land land = new Land(landMap.get("area").toString(),
-                landMap.get("assingment").toString());
-        lrepository.save(land);
-
-        Map<String, Object> response = new LinkedHashMap<String, Object>();
-        response.put("message", "land created successfully");
-        response.put("land", land);
-        return response;
+    public ResponseEntity<?> createLand(@RequestBody Map<String, Object> landMap){
+//        Land land = new Land(landMap.get("area").toString(),
+//                landMap.get("assingment").toString());
+//        lrepository.save(land);
+//
+//        Map<String, Object> response = new LinkedHashMap<String, Object>();
+//        response.put("message", "land created successfully");
+//        response.put("land", land);
+//        return response;
+        String area = landMap.get("area").toString();
+        String assignment = landMap.get("assignment").toString();
+        //JSONObject response = new JSONObject();
+        //response.put("message", "land created successfully");
+        return new ResponseEntity<>(lrepository.save(new Land(area, assignment)), HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "{/landId}")
-    public Land getLandDetails(@PathVariable("landId") String landId) {
-        return lrepository.findOne(landId);
+    @RequestMapping(method = RequestMethod.GET, value = "/{landId}")
+    public ResponseEntity<?> getLandDetails(@PathVariable("landId") String landId) {
+        //JSONObject response = new JSONObject();
+        return new ResponseEntity<>(lrepository.findOne(landId), HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "{/landId}")
-    public Map<String, Object> editLand(@PathVariable("landId") String landId, @RequestBody Map<String, Object> landMap){
+    @RequestMapping(method = RequestMethod.PUT, value = "/{landId}")
+    public ResponseEntity<?> editLand(@PathVariable("landId") String landId, @RequestBody Map<String, Object> landMap){
         Land land = new Land(landMap.get("area").toString(),
-                landMap.get("assingment").toString());
+                landMap.get("assignment").toString());
         land.setId(landId);
 
-        Map<String, Object> response = new LinkedHashMap<String, Object>();
-        response.put("message", "land updates successfully");
+//        Map<String, Object> response = new LinkedHashMap<String, Object>();
+//        response.put("message", "land updates successfully");
+//        response.put("land", lrepository.save(land));
+//        return response;
+        JSONObject response = new JSONObject();
+        response.put("message", "land updated successfully");
         response.put("land", lrepository.save(land));
-        return response;
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "{/landId}")
-        public Map<String, Object> deleteLand(@PathVariable("landId") String landId){
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{landId}")
+        public ResponseEntity<?> deleteLand(@PathVariable("landId") String landId){
         lrepository.delete(landId);
-        Map<String, Object> response = new HashMap<String, Object>();
+//        Map<String, Object> response = new HashMap<String, Object>();
+//        response.put("message", "land deleted successfully");
+//        return response;
+        JSONObject response = new JSONObject();
         response.put("message", "land deleted successfully");
-        return response;
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.GET)
-         public Map<String, Object> getAllLand(){
+         public ResponseEntity<?> getAllLand(){
          List<Land> lands = lrepository.findAll();
-         Map<String, Object> response = new LinkedHashMap<String, Object>();
-         response.put("totalLands", lands.size());
-         response.put("lands", lands);
-         return response;
+//         Map<String, Object> response = new LinkedHashMap<String, Object>();
+//         response.put("totalLands", lands.size());
+//         response.put("lands", lands);
+//         return response;
+        JSONObject response = new JSONObject();
+        response.put("totalLands", lands.size());
+        response.put("lands", lands);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
--- src/main/java/landmarketmavenproto/controller/SellerController.java
+++ src/main/java/landmarketmavenproto/controller/SellerController.java
@@ -49,13 +49,17 @@ public class SellerController {
         return new ResponseEntity<>(srepository.save(new Seller(login, password)), HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "{/sellerId}")
-    public Seller getSellerDetails(@PathVariable("sellerId") String sellerId) {
-        return srepository.findOne(sellerId);
+    @RequestMapping(method = RequestMethod.GET, value = "/{sellerId}")
+    public ResponseEntity<?> getSellerDetails(@PathVariable(value = "sellerId") String sellerId) {
+        JSONObject response = new JSONObject();
+        response.put("message", "seller details");
+        response.put("seller", srepository.findOne(sellerId));
+        return new ResponseEntity<>(response, HttpStatus.OK);
+        //return srepository.findOne(sellerId);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "{/sellerId}")
-    public Map<String, Object> editSeller(@PathVariable("sellerId") String sellerId, @RequestBody Map<String, Object> sellerMap) {
+    @RequestMapping(method = RequestMethod.PUT, value = "/{sellerId}")
+    public ResponseEntity<?> editSeller(@PathVariable(value = "sellerId") String sellerId, @RequestBody Map<String, Object> sellerMap) {
 //            Seller seller = new Seller(sellerMap.get("passport").toString(),
 //                    sellerMap.get("companyName").toString(),
 //                    sellerMap.get("phone").toString(),
@@ -71,28 +75,40 @@ public class SellerController {
                 sellerMap.get("password").toString()
         );
         seller.setId(sellerId);
+//
+//        Map<String, Object> response = new LinkedHashMap<String, Object>();
+//        response.put("message", "Seller updated successfully");
+//        response.put("seller", srepository.save(seller));
+//        return response;
 
-        Map<String, Object> response = new LinkedHashMap<String, Object>();
+        JSONObject response = new JSONObject();
         response.put("message", "Seller updated successfully");
         response.put("seller", srepository.save(seller));
-        return response;
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "{/selerId}")
-    public Map<String, Object> deleteSeller(@PathVariable("sellerId") String sellerId) {
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{sellerId}")
+    public ResponseEntity<?> deleteSeller(@PathVariable(value = "sellerId") String sellerId) {
         srepository.delete(sellerId);
-        Map<String, Object> response = new HashMap<String, Object>();
-        response.put("message", "Seller deleted successfully");
-        return response;
+        JSONObject response = new JSONObject();
+        response.put("message", "seller deleted successfully");
+        return new ResponseEntity<>(response, HttpStatus.OK);
+//        Map<String, Object> response = new HashMap<String, Object>();
+//        response.put("message", "Seller deleted successfully");
+//        return response;
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public Map<String, Object> getAllSellers() {
+    public ResponseEntity<?> getAllSellers() {
         List<Seller> sellers = srepository.findAll();
-        Map<String, Object> response = new LinkedHashMap<String, Object>();
+        JSONObject response = new JSONObject();
         response.put("totalSellers", sellers.size());
         response.put("sellers", sellers);
-        return response;
+        return new ResponseEntity<>(response, HttpStatus.OK);
+//        Map<String, Object> response = new LinkedHashMap<String, Object>();
+//        response.put("totalSellers", sellers.size());
+//        response.put("sellers", sellers);
+//        return response;
     }
 
 
