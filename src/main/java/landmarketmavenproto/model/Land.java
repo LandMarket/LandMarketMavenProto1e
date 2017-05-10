4229330a448ecde22f514119fc2f3f9c1ec45ea7
Vitya Ezersky <vitya.ezersky95@mail.ru>
2017-05-10 11:54:12 +0300

    new12

--- src/main/java/landmarketmavenproto/model/Land.java
+++ src/main/java/landmarketmavenproto/model/Land.java
@@ -10,16 +10,13 @@ public class Land {
     private String area;
     private String assignment;
     private String price;
-    private String description;
-    private String address;
-    private String owner;
+//    private String description;
+//    private String address;
+//    private String owner;
     public Land() {
     }
 
-    public Land(String owner,String address, String area, String assignment, String price,String description) {
-        this.description = description;
-        this.owner = owner;
-        this.address =  address;
+    public Land( String area, String assignment, String price) {
         this.area = area;
         this.assignment = assignment;
         this.price = price;
@@ -41,35 +38,18 @@ public class Land {
         this.area = area;
     }
 
-    public String getAssignment() {
-        return assignment;
-    }
-
-    public void setAssignment(String assignment) {
-        this.assignment = assignment;
-    }
-    public String getPrice(){
-        return price;
-    }
-    public void setPrice(String price) {
-        this.price = price;
-    }
-
-    public String getOwner() {
-        return owner;
-    }
-
-    public void setOwner(String owner) {
-        this.owner = owner;
-    }
-    public String getDescription(){
-        return description;
-    }
-    public void setDescription(String description){
-        this.description =  description;
-    }
-    public String getAddress( String address){
-        return address;
-    }
+//    public String getAssignment() {
+//        return assignment;
+//    }
+//
+//    public void setAssignment(String assignment) {
+//        this.assignment = assignment;
+//    }
+//    public String getPrice(){
+//        return price;
+//    }
+//    public void setPrice(String price) {
+//        this.price = price;
+//    }
 }
 
