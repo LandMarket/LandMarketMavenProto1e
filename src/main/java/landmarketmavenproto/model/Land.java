894e1959000ac3f1800dee4ecf030795d74a1c79
Vitya Ezersky <vitya.ezersky95@mail.ru>
2017-05-10 14:31:26 +0300

    new1

--- src/main/java/landmarketmavenproto/model/Land.java
+++ src/main/java/landmarketmavenproto/model/Land.java
@@ -10,16 +10,19 @@ public class Land {
     private String area;
     private String assignment;
     private String price;
-//    private String description;
-//    private String address;
-//    private String owner;
+    private String description;
+    private String address;
+    private String owner;
     public Land() {
     }
 
-    public Land( String area, String assignment, String price) {
+    public Land( String area, String assignment, String price, String description, String address, String owner ) {
         this.area = area;
         this.assignment = assignment;
         this.price = price;
+        this.description= description;
+        this.address= address;
+        this.owner=owner;
     }
 
     public String getId() {
@@ -38,18 +41,36 @@ public class Land {
         this.area = area;
     }
 
-//    public String getAssignment() {
-//        return assignment;
-//    }
-//
-//    public void setAssignment(String assignment) {
-//        this.assignment = assignment;
-//    }
-//    public String getPrice(){
-//        return price;
-//    }
-//    public void setPrice(String price) {
-//        this.price = price;
-//    }
+    public String getAssignment() {
+        return assignment;
+    }
+
+    public void setAssignment(String assignment) {
+        this.assignment = assignment;
+    }
+    public String getPrice(){
+        return price;
+    }
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public void setDescription(String description){
+        this.description=description;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public void setAddress(String address){
+        this.address= address;
+    }
+    public String getOwner(){
+        return owner;
+    }
+    public void setOwner(String owner){
+        this.owner=owner;
+    }
 }
 
