ad8511fa183f388c3f3306a0067d820ae38cb1a3
Vitya Ezersky <vitya.ezersky95@mail.ru>
2017-05-16 16:14:14 +0300

    work1

--- src/main/java/landmarketmavenproto/model/Seller.java
+++ src/main/java/landmarketmavenproto/model/Seller.java
@@ -17,33 +17,20 @@ public class Seller {
     private String login;
     @Field("password")
     private String password;
-    @Field("passport")
+    @Field("confirm")
     private String confirm;
-    //@Field("companyName")
-  //  private String companyName;
-    //@Field("phone")
-   // private String phone;
-    //@Field("address")
-   // private String address;
     @Field("email")
     private String email;
-    //@Field("managerName")
-    //private String managerName;
-    //@Field("skype")
-    //private String skype;
+
 
     public Seller() {
     }
 
-    public Seller(String login,String email, String password, String confirm) {
+    public Seller(String login, String email, String password, String confirm) {
         this.login = login;
+        this.email = email;
         this.password = password;
         this.confirm = confirm;
-       // this.companyName = companyName;
-          this.email = email;
-//        this.managerName = managerName;
-//        this.skype = skype;
-//        this.password = password;
     }
 
 
@@ -63,54 +50,6 @@ public class Seller {
         this.confirm = confirm;
     }
 
-//    public String getCompanyName() {
-//        return companyName;
-//    }
-//
-//    public void setCompanyName(String companyName) {
-//        this.companyName = companyName;
-//    }
-//
-//    public String getPhone() {
-//        return phone;
-//    }
-//
-//    public void setPhone(String phone) {
-//        this.phone = phone;
-//    }
-//
-//    public String getAddress() {
-//        return address;
-//    }
-//
-//    public void setAddress(String address) {
-//        this.address = address;
-//    }
-
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-
-//    public String getManagerName() {
-//        return managerName;
-//    }
-//
-//    public void setManagerName(String managerName) {
-//        this.managerName = managerName;
-//    }
-//
-//    public String getSkype() {
-//        return skype;
-//    }
-//
-//    public void setSkype(String skype) {
-//        this.skype = skype;
-//    }
-
     public String getLogin() {
         return login;
     }
@@ -125,5 +64,12 @@ public class Seller {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
