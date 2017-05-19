6b47743794dfa3f77c318c23adbc3aed80a0ae02
Xafeon <nikita.korostelov@gmail.com>
2017-05-19 16:39:40 +0300

    Eeeeeeeeeeeeeeeee........

--- src/main/java/landmarketmavenproto/controller/LandController.java
+++ src/main/java/landmarketmavenproto/controller/LandController.java
@@ -1,7 +1,6 @@
 package landmarketmavenproto.controller;
 
 import landmarketmavenproto.model.Land;
-import landmarketmavenproto.model.Seller;
 import landmarketmavenproto.repository.LandRepository;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
